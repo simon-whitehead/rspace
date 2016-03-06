7e7aca24b4ad7e89e3c8db0048fbbf50e0a8397c
Simon Whitehead <chemnova@gmail.com>
2016-03-06 17:06:25 +1100

    Moved the default scene out of the main loop

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod game;
 fn main() {
     let mut window = engine::Window::new("RSpace", 800, 600);
 
+    window.set_scene(Box::new(game::scenes::GameScene));
+
     loop {
         if !window.process() {
             break;
         }
-
-        window.set_scene(Box::new(game::scenes::GameScene));
 
         window.render();
     }
